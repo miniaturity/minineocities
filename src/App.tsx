c345d9ac2ce0adf1c913581df6ab8e879b55246c
miniaturity <miniaturity2.o@gmail.com>
2025-07-22 22:51:17 -0400

    Statuses (1/2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 
-
 function App() {
 
 
@@ -16,31 +15,31 @@ function App() {
           </div>
 
           <div className="left-1">
-            <div className="l1-contents">
-              s
+            <div className="l1-contents"> {/* Status */}
+              <StatusBox />
             </div>
           </div>
 
           <div className="left-2">
-            <div className="l2-contents">
+            <div className="l2-contents"> {/* Me */}
               s
             </div>
           </div>
 
           <div className="middle-1">
-            <div className="m1-contents">
+            <div className="m1-contents"> {/* Main */}
               Hello. I am miniaturity. Welcome to my website. Hello.
             </div>
           </div>
 
           <div className="right-1">
-            <div className="r1-contents">
+            <div className="r1-contents"> {/* Contact */}
               s
             </div>
           </div>
 
           <div className="right-2">
-            <div className="r2-contents">
+            <div className="r2-contents"> {/* Projects */}
               s
             </div>
           </div>
@@ -53,31 +52,33 @@ function App() {
   );
 }
 
-const statusBox: React.FC = () => {
-
-  return (
-    <>
-      
-    </>
-  );
-}
-
 interface Status {
-  author: String,
   content: String,
   date: String,
+}
+
+const StatusBox: React.FC = () => {
+  const statuses: Status[] = require('./statuses.json');  
+
+  return (
+    <div className="status-box">
+      {statuses.map(s => (
+        <StatusItem status={s} />
+      ))}
+    </div>
+  );
 }
 
 interface StatusProps {
   status: Status
 };
 
-const status: React.FC<StatusProps> = ({ status }) => {
+const StatusItem: React.FC<StatusProps> = ({ status }) => {
   return (
-    <div className="status-contaiiner">
+    <div className="status-container">
       <div className="s-title">
         <div className="s-author">
-          {status.author}
+          {`mini (@miniaturity)`}
         </div>
         <div className="s-date">
           {`(${status.date})`}

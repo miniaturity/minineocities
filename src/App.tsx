fbe6aa7ecfbe4c19368e624b05e25defa39e4816
miniaturity <miniaturity2.o@gmail.com>
2025-07-22 22:29:14 -0400

    Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <div className="middle-frame">
+        
+        <div className="grid">
+
+          <div className="header-0">
+            //  mini aturi ty\\
+          </div>
+
+          <div className="left-1">
+            <div className="l1-contents">
+              s
+            </div>
+          </div>
+
+          <div className="left-2">
+            <div className="l2-contents">
+              s
+            </div>
+          </div>
+
+          <div className="middle-1">
+            <div className="m1-contents">
+              Hello. I am miniaturity. Welcome to my website. Hello.
+            </div>
+          </div>
+
+          <div className="right-1">
+            <div className="r1-contents">
+              s
+            </div>
+          </div>
+
+          <div className="right-2">
+            <div className="r2-contents">
+              s
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+const statusBox: React.FC = () => {
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+interface Status {
+  author: String,
+  content: String,
+  date: String,
+}
+
+interface StatusProps {
+  status: Status
+};
+
+const status: React.FC<StatusProps> = ({ status }) => {
+  return (
+    <div className="status-contaiiner">
+      <div className="s-title">
+        <div className="s-author">
+          {status.author}
+        </div>
+        <div className="s-date">
+          {`(${status.date})`}
+        </div>
+      </div>
+      <div className="s-content">
+        {status.content}
+      </div>
+      <div className="s-buttons">
+        
+      </div>
     </div>
   );
 }
